5c2ebd551795f1cbac33d826710539d141cbacf9
23bimal23 <bimal23paudel777@gmail.com>
2023-05-02 07:32:53 +0545

    added menubar

--- app/src/main/java/com/example/mobile_programming/LoginActivity.java
+++ app/src/main/java/com/example/mobile_programming/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.mobile_programming;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,72 +18,94 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText editEmail, editPassword;
-    private AppCompatButton btnLogin;
-    private Toolbar toolbar;
-    private TextView txtForget, txtSignup, txtWelcome;
+    private EditText edtEmail, edtPass;
+    private AppCompatButton btnSignIn;
 
+    private TextView txtBCA, txtWelcome, txtForgot, txtAlready, txtSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear_login_layout);
-        initToolbar();
-        findViews();
+
+        findView();
+
     }
 
-    private void initToolbar(){
-        toolbar=findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setTitle("Login Page");
-    }
-    private void findViews() {
-        txtWelcome = findViewById(R.id.txtWelcome);
-        editEmail = findViewById(R.id.editEmail);
-        editPassword = findViewById(R.id.editPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        txtForget = findViewById(R.id.txtForget);
-        txtSignup = findViewById(R.id.txtSignup);
-        btnLogin.setOnClickListener(this);
-        txtSignup.setOnClickListener(this);
 
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+    private void findView() {
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
+        txtForgot = findViewById(R.id.txtForgot);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        txtSignUp = findViewById(R.id.txtSignUp);
+        txtBCA = findViewById(R.id.txtBCA);
+        btnSignIn.setOnClickListener(this);
+        txtSignUp.setOnClickListener(this);
+//        btnSignIn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                if(!editEmail.getText().toString().isEmpty()&& !editPassword.getText().toString().isEmpty()){
-//
-//                }
+//                if(edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty())
+//                    ;
 //            }
 //        });
+
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.btnLogin) {
-        } else if (view.getId() == R.id.txtSignup) {
-            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-            intent.putExtra("Email",editEmail.getText().toString().trim());
-            intent.putExtra("Email","Email Address");
+        if (view.getId() == R.id.btnSignIn) {
+            if(edtEmail.getText().toString().isEmpty() || edtPass.getText().toString().isEmpty()){
+
+                if (edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty() ){
+                    Toast.makeText(this, "Please enter Email", Toast.LENGTH_LONG).show();
+                }
+                else if (edtPass.getText().toString().isEmpty() && !edtEmail.getText().toString().isEmpty() ){
+                    Toast.makeText(this, "Please enter Password", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Please enter Email and Password", Toast.LENGTH_LONG).show();
+
+                }
+            }
+            else if (!edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty()) {
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                Toast.makeText(this, "You have logged in", Toast.LENGTH_LONG).show();
+//            intent.putExtra("Email",edtEmail.getText().toString().trim());
+//                intent.putExtra("Email", "Email Address");
+                startActivity(intent);
 //            startActivityForResult(intent,100);
-//            startActivity(intent);
-//            finish();
-            signupActivityLauncher.launch(intent);
+
+
+            }
+
+
+
+        } else if (view.getId() == R.id.txtSignUp) {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+//            intent.putExtra("Email",edtEmail.getText().toString().trim());
+//            intent.putExtra("Email", "Email Address");
+            startActivity(intent);
+//            startActivityForResult(intent,100);
+//            signupActivityLauncher.launch(intent);
+
+
         }
     }
+
     ActivityResultLauncher<Intent> signupActivityLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode()== Activity.RESULT_OK){
-                        //There are no result code
+                    if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent data = result.getData();
                         Toast.makeText(LoginActivity.this, data.getStringExtra("Result"), Toast.LENGTH_SHORT).show();
                     }
                 }
-            });
+            }
+
+    );
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
--- app/src/main/java/com/example/mobile_programming/SignupActivity.java
+++ app/src/main/java/com/example/mobile_programming/SignupActivity.java
@@ -2,34 +2,123 @@ package com.example.mobile_programming;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class SignupActivity extends AppCompatActivity {
-String email="";
+import com.example.mobile_programming.R;
 
-AppCompatButton btnSignUP;
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    private TextView txtAccountSignIn;
+    private EditText edtName,edtEmail,edtPass,edtConfPass;
+    private CheckBox boxTerms;
+    private Toolbar toolbar;
+    AppCompatButton btnSignUp;
+    String email = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
-        btnSignUP=findViewById(R.id.btnSignUP);
-        email=getIntent().getStringExtra("Email");
-        Log.d("Email Address", email);
-   Toast.makeText(this, "email", Toast.LENGTH_SHORT).show();
-   btnSignUP.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-           Intent intent= new Intent();
-           intent.putExtra("Result","Result Return");
-           setResult(RESULT_OK,intent);
-           finish();
-       }
-   });
+        setContentView(R.layout.signup_layout);
+
+        //        email=getIntent().getStringExtra("Email");
+        //        Toast.makeText(this, "email", Toast.LENGTH_SHORT).show();
+        Log.d("Email Address",email);
+        initToolbar();
+        findview();
+        //        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        //            @Override
+        //            public void onClick(View view) {
+        //
+        //                    Intent intent = new Intent();
+        //                    intent.putExtra("Result", "Result Return");
+        //                    setResult(RESULT_OK, intent);
+        //                    finish();
+        //
+        //
+        //                }
+        //
+        //        });
+        //
     }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("Login Page");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void findview(){
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
+        edtConfPass = findViewById(R.id.edtConfPass);
+        boxTerms = findViewById(R.id.boxTerms);
+        btnSignUp  = findViewById(R.id.btnSignUp);
+        txtAccountSignIn = findViewById(R.id.txtAccountSignIn);
+        btnSignUp.setOnClickListener(this);
+        txtAccountSignIn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnSignUp) {
+            if (edtName.getText().toString().isEmpty() || edtEmail.getText().toString().isEmpty() || edtPass.getText().toString().isEmpty() || edtConfPass.getText().toString().isEmpty()) {
+
+                if (edtName.getText().toString().isEmpty() && !edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty() && !edtConfPass.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Please enter Name", Toast.LENGTH_SHORT).show();
+                } else if (!edtName.getText().toString().isEmpty() && edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty() && !edtConfPass.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Please enter Email", Toast.LENGTH_LONG).show();
+                }
+                else if (!edtName.getText().toString().isEmpty() && !edtEmail.getText().toString().isEmpty() && edtPass.getText().toString().isEmpty() && !edtConfPass.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Please enter Password", Toast.LENGTH_LONG).show();
+                }
+                else if (!edtName.getText().toString().isEmpty() && !edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty() && edtConfPass.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Please enter Confirm Password", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(this, "Please fill out all the fields", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+            else if (!(edtName.getText().toString().isEmpty() && edtEmail.getText().toString().isEmpty() && edtPass.getText().toString().isEmpty() && edtConfPass.getText().toString().isEmpty())){
+                if(edtPass.getText().toString().equals(edtConfPass.getText().toString())){
+                    Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(this, "Redirected from SignUp Button", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(this, "Password and Confrim Password didn't match", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        }
+        if (view.getId() == R.id.txtAccountSignIn) {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            Toast.makeText(this, "Redirected from SignUp Page", Toast.LENGTH_LONG).show();
+
+            startActivity(intent);
+
+        }
+    }
+
 }
+
